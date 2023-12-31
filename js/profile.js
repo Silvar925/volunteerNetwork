b412dd84afd71363292c99bb6944bd77cc1ad10b
Silvar925 <silvarmoonlight@gmail.com>
2023-12-31 17:36:01 +0300

    V2.0.2

--- js/profile.js
+++ js/profile.js
@@ -1,8 +1,14 @@
 const btnEditingProfile = document.getElementById("btnEditingProfile")
 const profileInfo_box = document.getElementById("profileInfo_box")
 const eitingProfile_box = document.getElementById("eitingProfile_box")
+const btnSave = document.getElementById("btnSave")
 
 btnEditingProfile.addEventListener('click', function() {
     profileInfo_box.classList.add("hidden")
     eitingProfile_box.classList.remove("hidden")
+})
+
+btnSave.addEventListener('click', function() {
+    eitingProfile_box.classList.add("hidden")
+    profileInfo_box.classList.remove("hidden")
 })
