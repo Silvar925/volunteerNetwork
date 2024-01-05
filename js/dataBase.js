f97cdf93dfd1213a649e9aa92db05d1736eb064b
Silvar925 <silvarmoonlight@gmail.com>
2024-01-05 14:14:27 +0300

    V2.0.8

--- js/dataBase.js
+++ js/dataBase.js
@@ -7,11 +7,33 @@ export let events = [
         "category": "Первая категория",
         "date": "03.12.2023",
         "numberParticipants": 256,
+        'address': 'Определенная выдуманная улица',
+
+        "programEvent": "Стать богатым и успешным — это долгосрочная задача, требующая усилий, целеустремленности и времени. Вот несколько общих рекомендаций, которые могут помочь вам в достижении этой цели:",
+
         "speakers": [
             1,
             2,
             3
         ],
+        "organizers": [
+            1
+        ]
+    },
+
+    {
+        "id": 3,
+        "name": "Тестовое мероприятие",
+        "text": "Добро пожаловать на Великое Событие \"Не похуй на Описание\"! Это мероприятие, где каждая деталь важна, и ни один гость не сможет сказать, что ему всё равно! Здесь даже мухи в восторге от увлекательных описаний.\r\n\r\nОщутите волнение, когда каждый шаг, каждое вдохновение описывается словами, которые тронут даже камень. Наши описания не оставят вас равнодушными, и даже стены будут говорить друг другу: \"Это было что-то невероятное!\"\r\n\r\nПрисоединяйтесь к нам на этом путешествии по словесному искусству, где нет места для \"пофигизма\", а каждое слово - как нота в симфонии великолепия!",
+        "photo": null,
+        "category": "Первая категория",
+        "date": "03.12.2023",
+        "numberParticipants": 256,
+        "programEvent": "Это другой текст",
+        "speakers": [
+            1
+        ],
+
         "organizers": [
             1
         ]
@@ -23,24 +45,90 @@ export let speaker = [
         "id": 1,
         "name": "Али Аджиев",
         "bio": "Ого. Не знаю, с чего начать. Али Аджиев - самый успешный предприниматель в КЧР. Он стал миллионером в 15 лет благодаря своему таланту, Sigma, Top G, и в 18 лет уже обладает выдающимся умом. Даже Эндрю Тейт признает его величие и приезжает в КЧ, чтобы встретиться с ним. Тейт говорит, что Али Аджиев - лучший человек, которого он знает, и что он - его первый ученик, а Тристан - второй.",
-        "photo": "http://127.0.0.1:8000/images/speakers/photo_2023-10-06_23-55-42.jpg"
+        "photo": "http://127.0.0.1:8000/images/speakers/photo_2023-10-06_23-55-42.jpg",
+        'post': "Индивидуальный предприниматель"
     },
     {
         "id": 2,
         "name": "Ислам Хубиев",
         "bio": "Этот пиздюк на столько успешный, что даже сам Илон Маск берете у него советы. Спрашивать что делать в такой сложной ситуации и Ислам ему не отвечает. Потому что он слишком хорош, что бы отвечать каким-то Илонам Маскам. Он общается с рыбами по крупнее. В свои 15 лет он имел всех девушек своего колледжа во всех смыслах. В свои 17 стал турецким миллионером.",
-        "photo": "http://127.0.0.1:8000/images/speakers/image.png"
+        "photo": "http://127.0.0.1:8000/images/speakers/image.png",
+        'post': "Индивидуальный предприниматель"
     },
     {
         "id": 3,
         "name": "Амир-Аббас Семенов aka Лось",
         "bio": "16-летнего музыкального гения! Его первый аккорд был так удивителен, что даже его соседи перестали выключать свою музыку. Он такой молодой, что его первый концерт проходил в столовой школы, и его главная аудитория - учебники и резиновые уточки в комнате. Тем не менее, он уже создал свой первый альбом \"Недрочабрь\". \r\nУтверждает что он бесмертен, потому что перерождается и что самую первую музыку создал он, обучил в прошлом многих великих музыкантов, в том числе классических. Говорит не слушайте поп рок, слушайте фонк.",
-        "photo": "http://127.0.0.1:8000/images/speakers/212.png"
+        "photo": "http://127.0.0.1:8000/images/speakers/212.png",
+        'post': "Индивидуальный предприниматель"
     },
     {
         "id": 4,
         "name": "Али Мамчуев",
         "bio": "123",
         "photo": null
+    }
+]
+
+export let organizer = [
+    {
+        "id": 1,
+        "name": "Чомаева Мадина",
+        "bio": "Молодая талантливая личность, родившаяся 26 ноября 2002 года в красивом уголке Казахстана. Её биография уже насыщена интересными событиями, а будущее обещает быть таким же ярким и увлекательным.\r\n\r\nС детства Мадина проявила свой интерес к миру искусства. Её родители всегда поддерживали её увлечения и помогали развивать творческий потенциал. Уже в раннем возрасте она начала заниматься музыкой и изобразительным искусством, покоряя своим талантом близких и учителей.",
+        "photo": "http://127.0.0.1:8000/images/speakers/gT42WFD8kNc.jpg",
+        'post': 'asd'
+    }
+]
+
+export let news = [
+    {
+        "id": 2,
+        "name": "Новость 1",
+        "text": "В глубине космоса танцевали светящиеся звезды, создавая невероятные узоры на черном фоне бескрайней вселенной. Среди этого беспредельного космического океана плыли космические корабли, пересекая просторы пространства и времени.На одной из отдаленных планет водружался закат, окрашивая небеса оттенками огненного пурпурного света. Существо, напоминающее существо из легенд, шагало по песчаным дюнам, оставляя светящиеся следы за собой.",
+        "photo": "http://127.0.0.1:8000/images/news/%D0%94%D0%BE%D1%80%D0%BE%D0%B3%D0%B0.jpeg",
+        "category": "Первая категория",
+        "date": "03.12.2023",
+        "numberParticipants": 256
+    },
+    {
+        "id": 3,
+        "name": "Новость 2",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in Vel pretium lectus quam id leo in vitae turpis. Vel quam elementum pulvinar etiam non. In aliquam sem fringilla ut morbi tincidunt augue interdum. Orci nulla pellentesque dignissim enim sit amet. Ac turpis egestas maecenas pharetra convallis posuere morbi. Faucibus nisl tincidunt eget nullam non nisi est sit. Pellentesque id nibh tortor id aliquet lectus proin nibh. Viverra suspendisse potenti nullam ac tortor. Urna id volutpat lacus laoreet non curabitur gravida. Metus dictum at tempor commodo ullamcorper a lacus vestibulum sed. Lacus luctus accumsan tortor posuere ac ut consequat. Arcu cursus euismod quis viverra nibh cras.\r\n   Aenean et tortor at risus viverra adipiscing at. Enim eu turpis egestas pretium aenean pharetra magna ac. Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor. Turpis egestas sed tempus urna. Habitant morbi tristique senectus et netus et malesuada fames ac. Viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Eu sem integer vitae justo eget. Orci eu lobortis elementum nibh tellus. Vivamus arcu felis bibendum ut tristique et egestas quis. In pellentesque massa placerat duis. Nam at lectus urna duis convallis convallis tellus id interdum. Egestas congue quisque egestas diam in arcu cursus euismod quis.\r\n   Pharetra massa massa ultricies mi quis hendrerit dolor magna. Lorem dolor sed viverra ipsum nunc aliquet. Gravida quis blandit turpis cursus in. Mi ipsum faucibus vitae aliquet nec. Massa ultricies mi quis hendrerit dolor. Velit dignissim sodales ut eu sem. Tortor vitae purus faucibus ornare suspendisse. Congue nisi vitae suscipit tellus mauris. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Tempus quam pellentesque nec nam aliquam sem et tortor. In fermentum et sollicitudin ac orci. Pellentesque eu tincidunt tortor aliquam",
+        "photo": "http://127.0.0.1:8000/images/news/%D0%94%D0%BE%D1%80%D0%BE%D0%B3%D0%B0.jpeg",
+        "category": "Первая категория",
+        "date": "03.12.2023",
+        "numberParticipants": 256
+    }
+]
+
+export let rating = [
+    {
+        'id' : 1,
+        'fullname' : 'Чомаева Мадина Халисовна',
+        'university' : 'СКГА',
+        'countEvents' : 154,
+        'alpha' : 100,
+        'omega' : 54, 
+        'points' : 154
+    },
+
+    {
+        'id' : 2,
+        'fullname' : 'Чомаева Мадина Халисовна',
+        'university' : 'СКГА',
+        'countEvents' : 154,
+        'alpha' : 100,
+        'omega' : 54, 
+        'points' : 154
+    },
+
+    {
+        'id' : 3,
+        'fullname' : 'Чомаева Мадина Халисовна',
+        'university' : 'СКГА',
+        'countEvents' : 154,
+        'alpha' : 100,
+        'omega' : 54, 
+        'points' : 154
     }
 ]
--- js/events.js
+++ js/events.js
@@ -1,22 +1,18 @@
-import { events, speaker } from "./dataBase.js"
+import { events, speaker, organizer } from "./dataBase.js"
+import { getAllTagsA, extractNumbersFromString, clearDiv } from "./helpers.js"
+
+document.getElementById('countResultEvent').textContent = events.length + " " + "результатов"
 
 const containerEvents = document.getElementById("section_events_grid")
 const sectionEventsInfo = document.getElementById("section_events_info")
-const btnNews = document.getElementById("btnNews")
+
 const btnBack = document.getElementById("btnBack")
-
-
-const container_events_cardList = document.getElementById('container_events_cardList')
-
-btnNews.addEventListener('click', function () {
-    containerEvents.classList.add("hidden")
-    sectionEventsInfo.classList.remove("hidden")
-})
-
 btnBack.addEventListener('click', function () {
+    clearDiv('container_eventPage_info_volunteersGrid');
     containerEvents.classList.remove("hidden")
     sectionEventsInfo.classList.add("hidden")
 })
+
 
 function addNewEventCard(id, name, category, date, numberParticipants) {
     let divCard = document.createElement('div')
@@ -40,16 +36,100 @@ function addNewEventCard(id, name, category, date, numberParticipants) {
     container_events_cardList.appendChild(divCard)
 
 }
+
 function addAllEvents(events) {
-    events = events[0];
-
-    const { id, name, category, date, numberParticipants } = events;
-
-    if (name && category && date && numberParticipants) {
-        addNewEventCard(id, name, category, date, numberParticipants);
+    for (let i = 0; i < events.length; i++) {
+        const { id, name, category, date, numberParticipants } = events[i];
+        if (name && category && date && numberParticipants) {
+            addNewEventCard(id, name, category, date, numberParticipants);
+        }
     }
 }
 
-
 addAllEvents(events)
 
+function addNewSpeakerOrOgranizer(name, post, or) {
+    const gridOrganizers = document.getElementById('gridOrganizers')
+    const divSpeaker = document.createElement('div')
+
+    const speakerGrid = document.getElementById('container_eventPage_info_volunteersGrid')
+    divSpeaker.classList.add('container_eventPage_info_volunteersGrid_card')
+
+    divSpeaker.innerHTML = `
+    <img src="/images/man_2.png" alt="#">
+
+    <div class="container_eventPage_info_volunteersGrid_card_info">
+        <h3>${name}</h3>
+        <p>${post}</p>
+    </div>`
+
+    if (or == 's') {
+        speakerGrid.appendChild(divSpeaker)
+    } else if (or == 'o') {
+        gridOrganizers.appendChild(divSpeaker)
+    }
+
+}
+
+let temp = getAllTagsA('container_events_cardList')
+
+function addEvent(pressedEvent, events) {
+    let idPressedEvent = extractNumbersFromString(pressedEvent.id)
+    const textEvent = document.getElementById('textEvent')
+    const numberParticipants = document.getElementById('numberParticipants')
+    const programEvent = document.getElementById('programEvent')
+    const dateСarryingEvent = document.getElementById('dateСarryingEvent');
+    const addressCarryingEvent = document.getElementById('addressCarryingEvent')
+
+    let speakersId = []
+    let organizersId = []
+
+    for (let key in events) {
+        if (idPressedEvent == events[key].id) {
+
+            speakersId = events[key].speakers
+            organizersId = events[key].organizers
+
+            textEvent.textContent = events[key].text
+            dateСarryingEvent.textContent = events[key].date
+            numberParticipants.textContent = "Количество участников: " + events[key].numberParticipants + " из " + 100
+
+            programEvent.textContent = events[key].programEvent
+            addressCarryingEvent.textContent = events[key].address
+        }
+    }
+
+    for (let key in speaker) {
+        for (let key2 in speakersId) {
+            if (speaker[key].id == speakersId[key2]) {
+                addNewSpeakerOrOgranizer(speaker[key].name, speaker[key].post, 's')
+            }
+        }
+    }
+
+    for (let key in organizer) {
+        for (let key2 in organizersId) {
+            if (organizer[key].id == organizersId[key2]) {
+                addNewSpeakerOrOgranizer(organizer[key].name, organizer[key].post, 'o')
+            }
+        }
+    }
+}
+
+function pressedNameEvent(temp) {
+    const titleNameEvent = document.getElementById('titleNameEvent')
+
+    for (let key in temp) {
+        temp[key].addEventListener('click', function () {
+            containerEvents.classList.add('hidden')
+            sectionEventsInfo.classList.remove('hidden')
+
+            titleNameEvent.textContent = temp[key].textContent
+
+            addEvent(temp[key], events)
+
+        })
+    }
+}
+
+pressedNameEvent(temp)
